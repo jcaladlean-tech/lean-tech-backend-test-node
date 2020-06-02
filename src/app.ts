6d20c20acb602f53e4a2f323bb2ff738ff8f0f7d
caladdd <juanpablo.calad@gmail.com>
2020-06-01 19:06:20 -0500

    Initial structure of backend enviroment

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import { config } from './config';
+
+class App {
+  public app: express.Application;
+  public mongoUrl: string = config.dataBase.URL;
+
+  constructor() {
+    this.app = express();
+    this.config();
+    this.mongoSetup();
+  }
+
+  private config(): void {
+    this.app.use(bodyParser.json());
+    this.app.use(bodyParser.urlencoded({ extended: false }));
+  }
+
+  private mongoSetup(): void {
+    mongoose.Promise = global.Promise;
+    mongoose.connect(this.mongoUrl, config.dataBase.options);
+  }
+}
+
+export default new App().app;
