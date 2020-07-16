0fb05d9f8922e37cbc5bcd6634374821c2b0259f
caladdd <juanpablo.calad@gmail.com>
2020-07-16 16:28:09 -0500

    Add index.html

--- src/services/HelloWorldServices.ts
+++ src/services/HelloWorldServices.ts
@@ -1,7 +1,24 @@
 import { Request, Response } from 'express';
+import fs from 'fs';
 
 export default class HelloWorldServices {
   public static routes(app: any) {
+    app.get('/', (req: Request, res: Response) => {
+      res.writeHead(200, {
+        'Content-Type': 'text/html'
+      });
+      fs.readFile('src/htmlTemplate/index.html', null, function (error, data) {
+        if (error) {
+          res.writeHead(404);
+          res.write('Whoops! File not found!');
+        } else {
+          res.write(data);
+        }
+        res.end();
+      });
+      // res.status(200).sendFile('src/htmlTemplate/index.html');
+    });
+
     app.get('/holamundo/txt', (req: Request, res: Response) => {
       res.status(200).send('Hello World!');
     });
